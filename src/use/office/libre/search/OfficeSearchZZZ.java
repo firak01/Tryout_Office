876674d65151151b0bba5ce19e4ae637a9499eb2
fl86kyvo <fl86kyvo@URZ-VWIT0-M011.adds.tu-freiberg.de>
2025-02-21 09:03:31 +0100

    Durchsuchen von in den ODT Dateien enthaltenes Content.xml

--- src/use/office/libre/search/OfficeSearchZZZ.java
+++ src/use/office/libre/search/OfficeSearchZZZ.java
@@ -4,12 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -24,7 +27,9 @@ import org.jdom2.input.SAXBuilder;
 public class OfficeSearchZZZ{
     private final Set<String> searchSet = new HashSet<>();
     private static OfficeSearchZZZ INSTANCE = new OfficeSearchZZZ();
-
+    
+    ArrayList<String>listasFile=null;
+    
     //
     // main
     //
@@ -62,7 +67,9 @@ public class OfficeSearchZZZ{
 
                         for (File file : files) {
                             if (isOdt(file)) {
-                                searchContent(file);
+                                //searchContentBodyText(file);
+                            	//searchContentStyle(file);
+                            	searchStylesStyle(file, false, false);
                             }
                         }
                     } else {
@@ -80,7 +87,7 @@ public class OfficeSearchZZZ{
        
 
     //
-    //  is_odt
+    //  isOdt
     //
     private boolean isOdt(File file) {
         if (file.isFile()) {
@@ -92,11 +99,182 @@ public class OfficeSearchZZZ{
         }
         return false;
     }
+    
+    public ArrayList<String> getInternalFilesSearchedFor(){
+    	if(listasFile==null) {
+    		listasFile=new ArrayList<String>();
+    		listasFile.add("content.xml");
+    	}    	
+    	return this.listasFile;
+    }
+    
+    public void setFilesSearchedFor(ArrayList<String> listasFile) {
+    	this.listasFile = listasFile;
+    }
 
+    private void searchStylesStyle(File odt) {
+    	searchStylesStyle_(odt, true, true);
+    }
+    
+    private void searchStylesStyle(File odt, boolean bContainsPhrase) {
+    	searchStylesStyle_(odt, bContainsPhrase, true);
+    }
+    
+    private void searchStylesStyle(File odt, boolean bContainsPhrase, boolean bExact ) {
+    	searchStylesStyle_(odt, bContainsPhrase, bExact);
+    }
+    
+    private void searchStylesStyle_(File odt, boolean bContainsPhrase, boolean bExact ) {
+    	ArrayList<String>listasFile=this.getInternalFilesSearchedFor();
+    			
+        try (ZipFile zip = new ZipFile(odt)) {
+        	
+        			
+            final ZipEntry content = zip.getEntry("content.xml");
+            if (content != null) {
+            	boolean bFoundAtFile = false;
+            	boolean bFoundAtElement = false;
+            	
+                final SAXBuilder builder = new SAXBuilder();
+                final Document doc = builder.build(zip.getInputStream(content));
+                final Element root = doc.getRootElement();
+                final Namespace office_ns = root.getNamespace("office");
+                final Namespace style_ns = root.getNamespace("style");
+                final Element automaticStyles = root.getChild("automatic-styles", office_ns);
+                if (automaticStyles != null) {                    
+                    for (Element e : automaticStyles.getDescendants(Filters.element(style_ns))) {
+                    		//final String sName = e.getName();  
+                    		//System.out.print("\n" + odt.toString() + " : Attribut: " + sName);
+                    				
+                    		//String sNameSpace = office_ns.getPrefix();
+                    		String sNameSpace = style_ns.getPrefix();
+                    		List<Attribute> listAttribute = e.getAttributes();
+                    		for(Attribute objAttribute: listAttribute) {                              
+                    			String sAttributeName = objAttribute.getName();
+                    			if(sAttributeName.equals("name")) {   
+                    				String sAttributeValue = objAttribute.getValue();
+	                    			for (String p : searchSet) {	   
+	                    				if(bContainsPhrase) {
+	                    					if(bExact) {
+				                                if (sAttributeValue.toLowerCase().contains(p)) {
+				                                        bFoundAtElement = true;
+				                                        bFoundAtFile = true;		                                       		                                       
+				                                }
+	                    					}else {
+	                    						if (sAttributeValue.contains(p)) {
+			                                        bFoundAtElement = true;
+			                                        bFoundAtFile = true;		                                       		                                       
+	                    						}
+			                                }
+	                    				}else {
+	                    					//!bContainsPhrase
+	                    					if(bExact) {
+				                                if (sAttributeValue.toLowerCase().equals(p)) {
+				                                        bFoundAtElement = true;
+				                                        bFoundAtFile = true;		                                       		                                       
+				                                }
+	                    					}else {
+	                    						if (sAttributeValue.equals(p)) {
+			                                        bFoundAtElement = true;
+			                                        bFoundAtFile = true;		                                       		                                       
+	                    						}
+			                                }
+	                    				}
+		                                
+		                                if(bFoundAtElement) {
+		                                	System.out.print("\n!!! " + odt.toString() + " : !!! Attribut: " + sAttributeName + "\t| Wert: " + sAttributeValue +" | found: " + p);
+		                                	bFoundAtElement=false;
+		                                }else {
+		                                	System.out.print("\n" + odt.toString() + " : Attribut: " + sAttributeName + "\t| Wert: " + sAttributeValue + " | but searched for: " + p);
+		                                }
+		                            }//end for
+                    			}//if(sAttributeName.equals("name")) {                    			                    		
+                    		}//end for                    		                    		                     
+                    }
+                }else{
+                	System.out.println("\n" + odt.toString() + " enthält keinen Knoten 'automatic-styles'.");                	
+                }//end if "Knoten in Struktur" gefunden.
+                
+                if(bFoundAtFile) {
+                	System.out.println("\n!!!" + odt.toString() + " enthält gesuchte(s) Attribut(e).");
+                }
+            }else {
+            	System.out.println("\n" + odt.toString() + " enthält keine Datei 'content.xml'.");
+            }//end if content!=null, d.h. "Datei" gefunden.
+        }
+        catch (IOException | JDOMException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    private void searchContentStyle(File odt) {
+        try (ZipFile zip = new ZipFile(odt)) {
+            final ZipEntry content = zip.getEntry("content.xml");
+            if (content != null) {
+            	boolean bFoundAtFile = false;
+            	boolean bFoundAtElement = false;
+            	
+                final SAXBuilder builder = new SAXBuilder();
+                final Document doc = builder.build(zip.getInputStream(content));
+                final Element root = doc.getRootElement();
+                final Namespace office_ns = root.getNamespace("office");
+                final Namespace style_ns = root.getNamespace("style");
+                final Element automaticStyles = root.getChild("automatic-styles", office_ns);
+                if (automaticStyles != null) {                    
+                    for (Element e : automaticStyles.getDescendants(Filters.element(style_ns))) {
+                    		//final String sName = e.getName();  
+                    		//System.out.print("\n" + odt.toString() + " : Attribut: " + sName);
+                    				
+                    		//String sNameSpace = office_ns.getPrefix();
+                    		String sNameSpace = style_ns.getPrefix();
+                    		List<Attribute> listAttribute = e.getAttributes();
+                    		for(Attribute objAttribute: listAttribute) {                              
+                    			String sAttributeName = objAttribute.getName();
+                    			if(sAttributeName.equals("name")) {                    				
+	                    			for (String p : searchSet) {	                    					                    				
+		                                if (sAttributeName.toLowerCase().contains(p)) {
+		                                        bFoundAtElement = true;
+		                                        bFoundAtFile = true;		                                       		                                       
+		                                }
+		                                
+		                                String sValue = objAttribute.getValue();
+		                                if(bFoundAtElement) {
+		                                	System.out.print("\n" + odt.toString() + " : !!! Attribut: " + sAttributeName + "\t| Wert: " + sValue);
+		                                	bFoundAtElement=false;
+		                                }else {
+		                                	System.out.print("\n" + odt.toString() + " : Attribut: " + sAttributeName + "\t| Wert: " + sValue);
+		                                }
+		                            }
+                    			}
+                    			
+                    			
+                    		}
+                    		
+                    		
+                     
+                    }
+                }else{
+                	System.out.println("\n" + odt.toString() + " enthält keinen Knoten 'automatic-styles'.");                	
+                }//end if "Knoten in Struktur" gefunden.
+                
+                if(bFoundAtFile) {
+                	System.out.println("\n!!!" + odt.toString() + " enthält gesuchte(s) Attribut(e).");
+                }
+            }else {
+            	System.out.println("\n" + odt.toString() + " enthält keine Datei 'content.xml'.");
+            }//end if content!=null, d.h. "Datei" gefunden.
+        }
+        catch (IOException | JDOMException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
     //
     // search
     //
-    private void searchContent(File odt) {
+    private void searchContentBodyText(File odt) {
         try (ZipFile zip = new ZipFile(odt)) {
             final ZipEntry content = zip.getEntry("content.xml");
             if (content != null) {
@@ -130,5 +308,8 @@ public class OfficeSearchZZZ{
             e.printStackTrace();
         }
     }
+    
+    
+   
 
 }
